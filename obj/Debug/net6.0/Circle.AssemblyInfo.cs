55dbbb518ff9633048f815da91030511286d0164
Noah Dyson <AuroraFlight12@gmail.com>
2024-06-02 12:54:09 +0100

    updated gitignore

--- obj/Debug/net6.0/Circle.AssemblyInfo.cs
+++ obj/Debug/net6.0/Circle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Circle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1e4c950cfeee816ac97605591ab9cf78b9bd786")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97d5aeefed70304ffd8aebad14c78cad77ce28c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Circle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Circle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
