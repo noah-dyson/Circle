09e677c47339fffdfdab1eed51638119797340aa
Noah Dyson <AuroraFlight12@gmail.com>
2024-06-02 13:22:32 +0100

    clean up moved to folders

--- obj/Debug/net6.0/Circle.AssemblyInfo.cs
+++ obj/Debug/net6.0/Circle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Circle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97d5aeefed70304ffd8aebad14c78cad77ce28c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55dbbb518ff9633048f815da91030511286d0164")]
 [assembly: System.Reflection.AssemblyProductAttribute("Circle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Circle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
